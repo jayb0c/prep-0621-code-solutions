aad5604fc14049534a2bc85b76d7eb2c6a330e6c
Jake Stone <Stonedev12@gmail.com>
2021-06-18 21:22:58 -0700

    Complete javascript for loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var x = 0; x < 10; x++) {
+  console.log(x * 2);
+}
+
+for (var y = 100; y >= 0; y--) {
+  console.log('Time till explosion: ' + y);
+}
